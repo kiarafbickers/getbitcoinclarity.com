c1604a984f25242c3d6f4853f079ea2fd4c717fb
Kiara <hi@kiararobles.com>
2019-10-24 10:58:59 -0500

    add faq are we a good fit page

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,3 +242,14 @@
 
 
 })(jQuery);
+
+// faq
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
